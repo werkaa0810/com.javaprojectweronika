6b7046a1511dc390eadc2fdfa1e47b3457810e5b
werkaa0810 <weronika07082001@gmail.com>
2021-02-16 17:20:59 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App {
+     public static void main(String[] args) throws Exception {  
+        Scanner scanner = new Scanner(System.in);
+        /*System.out.println("Podaj swoje imię:");
+     Scanner scanner = new Scanner(System.in);
+     String firstName = scanner.nextLine();
+     System.out.println("Twoje imię to "+ firstName);
+     scanner.close();
+     //Wpisywanie imienia i wyświetlanie go
+ */
+        /*System.out.println("Podaj pierwszą liczbę");
+        int a = scanner.nextInt();
+        System.out.println("Podaj drugą liczbę");
+        int b = scanner.nextInt();
+        System.out.println(a+" + " + b+" = "+(a+b));
+        */ 
+     }
+        int nr=2;
+        if(nr == 2){
+            System.out.println("Liczby są równe");
+        }else{System.out.println("Liczby nie są równe");}
+        scanner.close();
+}
